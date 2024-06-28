f5b1c5d529b19be8f3672eeaf8fb04b5061f1d49
NDTrung <ndtrung1998@gmail.com>
2024-06-28 18:10:57 +0700

    feat: first commit, all check passed, gh-pages added later

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 
-function App() {
+const quotesUrl =
+  "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
+
+const colors = [
+  "#16a085",
+  "#27ae60",
+  "#2c3e50",
+  "#f39c12",
+  "#e74c3c",
+  "#9b59b6",
+  "#FB6964",
+  "#342224",
+  "#472E32",
+  "#BDBB99",
+  "#77B1A9",
+  "#73A857",
+];
+
+function getRandom(array) {
+  const index = Math.floor(Math.random() * array.length);
+  return array[index];
+}
+
+const App = () => {
+  const [quote, setQuote] = useState({ quote: "", author: "" });
+  const [currentColor, setCurrentColor] = useState("rgb(24, 26, 43)");
+
+  useEffect(() => {
+    fetchRandomQuote(quotesUrl);
+  }, []);
+
+  async function fetchRandomQuote(url) {
+    try {
+      await fetch(url)
+        .then((res) => res.json())
+        .then((quotes) => {
+          setQuote({ ...getRandom(quotes.quotes) });
+          setCurrentColor(getRandom(colors));
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  function handleNewQuote(event) {
+    event.preventDefault();
+    fetchRandomQuote(quotesUrl);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundColor: currentColor }}>
+      <div id="quote-box">
+        <div id="text" style={{ color: currentColor }}>
+          {quote.quote}
+        </div>
+        <div id="author" style={{ color: currentColor }}>
+          {quote.author}
+        </div>
+        <div className="container container_h" id="btns-wrapper">
+          <a
+            href={`https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${encodeURIComponent(
+              '"' + quote.quote + '" ' + quote.author
+            )}`}
+            id="tweet-quote"
+            className="container icon-wrapper"
+            title="Tweet this quote"
+            style={{ backgroundColor: currentColor }}
+            target="_blank"
+          >
+            <FontAwesomeIcon
+              icon={faTwitter}
+              className="icon"
+              color={`white`}
+              class="icon"
+            />
+          </a>
+          <button
+            id="new-quote"
+            style={{ backgroundColor: currentColor }}
+            onClick={handleNewQuote}
+          >
+            New quote
+          </button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
